83c8e2f65303d4a93ed000633e95c6d65014c6a6
GuilleGarro01 <132840836+GuilleGarro01@users.noreply.github.com>
2023-05-07 16:50:26 -0300

    Add files via upload

---
+++ eje2/menu.py
@@ -0,0 +1,9 @@
+from Ejercicio2-u2 import Viajero
+class Menu:
+    __opc = ""
+    
+    def __init__(self):
+        __opc="" 
+    def MenuOpciones (self):
+        NumViajero= int(input("Ingrese numero de viajero"))
+        
